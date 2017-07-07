a8b6e671a713fe6dceedc219782a47496650612e
eric <eric@ericdeMacBook-Pro.local>
2017-07-07 16:41:14 +0800

    创建app

---
+++ appDemo/appDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  appDemo
+//
+//  Created by eric on 2017/7/7.
+//  Copyright © 2017年 eric. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
